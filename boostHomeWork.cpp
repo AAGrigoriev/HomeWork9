5b5fec84a180bc72776922ad5bd8c1b44ef42cfb
AAGrigoriev <grigormaklaud@gmail.com>
2020-09-09 22:14:36 +0300

    adding

--- boostHomeWork.cpp
+++ boostHomeWork.cpp
@@ -1,35 +1,93 @@
-#include "boost/program_options.hpp"
-#include "iostream"
+#include <boost/program_options.hpp>
 
-using namespace std;
-
+using namespace boost;
 namespace po = boost::program_options;
 
-int main(int argc, const char *argv[])
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+using namespace std;
+
+
+// A helper function to simplify the main part.
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v)
 {
+    copy(v.begin(), v.end(), ostream_iterator<T>(os, " "));
+    return os;
+}
 
-    po::options_description desc("Allowed options");
-    desc.add_options()("help", "produce help message")("compression", po::value<int>(), "set compression level");
+int main(int ac, char* av[])
+{
+    try {
+        int level_depth;
+        int file_size; // byte
+        int block_size;// byte
+        po::options_description desc("Allowed options");
+        desc.add_options()("help,h", "produce help message")
+        ("exclude,e",po::value< vector<string> >(),"list directory")
+        ("exception",po::value<vector<string>>(),"list exception direcotorty")
+        ("hash-func,f",po::value<string>(),"hash function md5 crc32")
+        ("level,l",po::value<int>(&level_depth),"restrict depth of search, negative value to no limit")
+        ("pattern,p",po::value<vector<string>>(),"regex to match file names, only matched will be processed")
+        ("min-file,m",po::value<int>(&file_size)->default_value(1),"minimum file size to process in bytes, may significantly affect performance");
+        ("block-size,b",po::value<int>(&block_size)->default_value(1),"block size for scanning")
 
-    po::variables_map vm;
-    po::store(po::parse_command_line(argc, argv, desc), vm);
-    po::notify(vm);
 
-    if (vm.count("help"))
+
+      /*  desc.add_options()
+            ("help", "produce help message")
+            ("optimization", po::value<int>(&opt)->default_value(10),
+                  "optimization level")
+            ("verbose,v", po::value<int>()->implicit_value(1),
+                  "enable verbosity (optionally specify level)")
+            ("listen,l", po::value<int>(&portnum)->implicit_value(1001)
+                  ->default_value(0,"no"),
+                  "listen on a port.")
+            ("include-path,I", po::value< vector<string> >(),
+                  "include path")
+            ("input-file", po::value< vector<string> >(), "input file")
+        ;
+*/
+        po::positional_options_description p;
+        p.add("input-file", -1);
+
+        po::variables_map vm;
+        po::store(po::command_line_parser(ac, av).
+                  options(desc).positional(p).run(), vm);
+        po::notify(vm);
+
+        if (vm.count("help")) {
+            cout << "Usage: options_description [options]\n";
+            cout << desc;
+            return 0;
+        }
+
+        if (vm.count("include-path"))
+        {
+            cout << "Include paths are: "
+                 << vm["include-path"].as< vector<string> >() << "\n";
+        }
+
+        if (vm.count("input-file"))
+        {
+            cout << "Input files are: "
+                 << vm["input-file"].as< vector<string> >() << "\n";
+        }
+
+        if (vm.count("verbose")) {
+            cout << "Verbosity enabled.  Level is " << vm["verbose"].as<int>()
+                 << "\n";
+        }
+
+        cout << "Optimization level is " << opt << "\n";
+
+        cout << "Listen port is " << portnum << "\n";
+    }
+    catch(std::exception& e)
     {
-        cout << desc << "\n";
+        cout << e.what() << "\n";
         return 1;
     }
-
-    if (vm.count("compression"))
-    {
-        cout << "Compression level was set to "
-             << vm["compression"].as<int>() << ".\n";
-    }
-    else
-    {
-        cout << "Compression level was not set.\n";
-    }
-
     return 0;
 }
