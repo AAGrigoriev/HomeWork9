7bc10ae903184e90de26180e5b540124dfe944cc
AAGrigoriev <grigormaklaud@gmail.com>
2020-09-21 21:34:34 +0300

    boost programm option

--- boostHomeWork.cpp
+++ boostHomeWork.cpp
@@ -1,93 +1,32 @@
+
 #include <boost/program_options.hpp>
-
-using namespace boost;
-namespace po = boost::program_options;
-
+#include <exception>
 #include <iostream>
-#include <algorithm>
-#include <iterator>
-using namespace std;
+#include <filesystem>
+#include <vector>
+#include <string>
 
-
-// A helper function to simplify the main part.
-template<class T>
-ostream& operator<<(ostream& os, const vector<T>& v)
-{
-    copy(v.begin(), v.end(), ostream_iterator<T>(os, " "));
-    return os;
-}
+namespace bpo = boost::program_options;
+namespace fs = std::filesystem;
 
 int main(int ac, char* av[])
 {
-    try {
-        int level_depth;
-        int file_size; // byte
-        int block_size;// byte
-        po::options_description desc("Allowed options");
-        desc.add_options()("help,h", "produce help message")
-        ("exclude,e",po::value< vector<string> >(),"list directory")
-        ("exception",po::value<vector<string>>(),"list exception direcotorty")
-        ("hash-func,f",po::value<string>(),"hash function md5 crc32")
-        ("level,l",po::value<int>(&level_depth),"restrict depth of search, negative value to no limit")
-        ("pattern,p",po::value<vector<string>>(),"regex to match file names, only matched will be processed")
-        ("min-file,m",po::value<int>(&file_size)->default_value(1),"minimum file size to process in bytes, may significantly affect performance");
-        ("block-size,b",po::value<int>(&block_size)->default_value(1),"block size for scanning")
+    int level;
+    int min_size;
+    int block_size;
+
+    bpo::options_description desc{"Options"};
+    desc.add_options()
+    ("include,i",bpo::value<std::vector<fs::path>>()->multitoken()->composing(),"Include path for scanning")
+    ("exclude,e",bpo::value<std::vector<fs::path>>()->multitoken()->composing(),"Exclude directory")
+    ("level,l",bpo::value<int>(&level)->default_value(-1),"Maximum depth of scan, -1 no limit")
+    ("min-size,s",bpo::value<int>(&min_size)->default_value(5),"Minimum file size in bytes")
+    ("mask,m",bpo::value<std::vector<std::string>>()->multitoken(),"Masks for filename")
+    ("block,b",bpo::value<int>(&block_size)->default_value(10),"Block size in bytes")
+    ("Hash,h",bpo::value<std::string>()->default_value("crc16"),"Hash algorithm")
+    ("Help,h","Help scrren");
 
 
 
-      /*  desc.add_options()
-            ("help", "produce help message")
-            ("optimization", po::value<int>(&opt)->default_value(10),
-                  "optimization level")
-            ("verbose,v", po::value<int>()->implicit_value(1),
-                  "enable verbosity (optionally specify level)")
-            ("listen,l", po::value<int>(&portnum)->implicit_value(1001)
-                  ->default_value(0,"no"),
-                  "listen on a port.")
-            ("include-path,I", po::value< vector<string> >(),
-                  "include path")
-            ("input-file", po::value< vector<string> >(), "input file")
-        ;
-*/
-        po::positional_options_description p;
-        p.add("input-file", -1);
-
-        po::variables_map vm;
-        po::store(po::command_line_parser(ac, av).
-                  options(desc).positional(p).run(), vm);
-        po::notify(vm);
-
-        if (vm.count("help")) {
-            cout << "Usage: options_description [options]\n";
-            cout << desc;
-            return 0;
-        }
-
-        if (vm.count("include-path"))
-        {
-            cout << "Include paths are: "
-                 << vm["include-path"].as< vector<string> >() << "\n";
-        }
-
-        if (vm.count("input-file"))
-        {
-            cout << "Input files are: "
-                 << vm["input-file"].as< vector<string> >() << "\n";
-        }
-
-        if (vm.count("verbose")) {
-            cout << "Verbosity enabled.  Level is " << vm["verbose"].as<int>()
-                 << "\n";
-        }
-
-        cout << "Optimization level is " << opt << "\n";
-
-        cout << "Listen port is " << portnum << "\n";
-    }
-    catch(std::exception& e)
-    {
-        cout << e.what() << "\n";
-        return 1;
-    }
     return 0;
 }
